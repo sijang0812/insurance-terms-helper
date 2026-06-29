41afd1751baef31c303f26d61068475d8225bd3d
sijang0812 <sijang0812@gmail.com>
2026-06-29 23:23:54 +0900

    fix: PathVariable import 복구

--- backend/document-service/src/main/java/com/iht/document/controller/DocumentController.java
+++ backend/document-service/src/main/java/com/iht/document/controller/DocumentController.java
@@ -7,6 +7,7 @@ import com.iht.document.dto.DocumentUploadResponse;
 import com.iht.document.service.DocumentService;
 import com.iht.document.store.UploadProgressStore;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
